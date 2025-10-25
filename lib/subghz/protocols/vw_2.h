0f160989c3112ab8dfa23f162d79baad4c50ee07
Vencislav Atanasov <git@6bez10.info>
2025-10-25 18:52:39 +0300

    Move AUT64 encrypt/decrypt code to protocol

--- lib/subghz/protocols/vw_2.h
+++ lib/subghz/protocols/vw_2.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <stdint.h>
+
+#define AUT64_NUM_ROUNDS 12
+#define AUT64_BLOCK_SIZE 8
+#define AUT64_KEY_SIZE 8
+#define AUT64_PBOX_SIZE 8
+#define AUT64_SBOX_SIZE 16
+
+struct aut64_key {
+    uint8_t key[AUT64_KEY_SIZE];
+    uint8_t pbox[AUT64_PBOX_SIZE];
+    uint8_t sbox[AUT64_SBOX_SIZE];
+};
+
+void aut64_encrypt(const struct aut64_key key, uint8_t message[]);
+void aut64_decrypt(const struct aut64_key key, uint8_t message[]);
+
 #include "base.h"
 
 #define SUBGHZ_PROTOCOL_VW_2_NAME "VW-2"
